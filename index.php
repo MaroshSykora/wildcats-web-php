83b098b6ab26024fd4ad6c4d09aee50f7b417474
Marosh Sykora <sykoramaros@gmail.com>
2024-09-03 14:59:18 +0200

    pridano pole zvirat do index.php

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+$animals = [
+  [
+    "animal_name" => "Cougar",
+    "latin_name" => "Puma concolor",
+    "distribution" => "America",
+  ],
+  [
+    "animal_name" => "Amur leopard",
+    "latin_name" => "Panthera pardus orientalis",
+    "distribution" => "Asia",
+  ],
+  [
+    "animal_name" => "Jaguar",
+    "latin_name" => "Panthera onca",
+    "distribution" => "America",
+  ],
+  [
+    "animal_name" => "Eurasian lynx",
+    "latin_name" => "Lynx lynx",
+    "distribution" => "Europe",
+  ],
+];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Wild Cats</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
